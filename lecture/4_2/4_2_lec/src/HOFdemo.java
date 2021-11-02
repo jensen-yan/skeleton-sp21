04ebe5edde45345e941bce0f4f7c49f151e836d8
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-11-02 21:40:04 +0800

    add lecture 4_2

--- lecture/4_2/4_2_lec/src/HOFdemo.java
+++ lecture/4_2/4_2_lec/src/HOFdemo.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class HOFdemo {
+public class HOFdemo {
+
+    public static int do_twice(IntUnaryFunction f, int x){
+        return f.apply(f.apply(x));
+    }
+
+    public static void main(String[] args) {
+        TenX tenx = new TenX();
+        int o = do_twice(tenx, 2);
+        System.out.println(o);
+    }
 }
